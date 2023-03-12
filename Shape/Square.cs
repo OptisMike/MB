84b8341acf1b600faa543ba7c7c81a2b9c07b7b5
OptisMike <Kokorin.huawei@gmail.com>
2023-03-12 12:05:42 +0300

    Добавление функции высчитывающей площадь круга

--- Shape/Square.cs
+++ Shape/Square.cs
@@ -6,8 +6,46 @@ using System.Threading.Tasks;
 
 namespace Square
 {
-    public class Squre<T>
+    static public class Squre
     {
+        static public double Calculate(params double[] side)
+        {
+            switch (side.Length) 
+            {
+                case 1:
+                    return CircleCalculate(side[0]);
+                case 3:
+                    return TriangleCalculate(side[0], side[1], side[2]);
+                default:
+                    return 0;
+            }
+        }
 
+        static public double CircleCalculate(double radius)
+        {
+            if (IsSidesGreaterZero(radius))
+                return Math.PI * Math.Pow(radius, 2);
+            return 0;
+        }
+
+        static public double TriangleCalculate(double firstSide, double secondSide, double thirdSide)
+        {
+            return 0;
+        }
+
+        static private bool IsTriangleExist()
+        {
+
+        }
+
+        static private bool IsSidesGreaterZero(params double[] sides)
+        {
+            foreach (var side in sides)
+            {
+                if (side <= 0)
+                    return false;
+            }
+            return true;
+        }
     }
 }
